310e7ff888d80c36ec163f12a074d224cedcf647
sibylledL <33783030+sibylledL@users.noreply.github.com>
2018-03-16 10:48:41 +0100

    premier commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # calculatrice
 JS/HTML/CSS
+
+
+faire une calculatrice qui puisse faire les opérations basiques 
+sans prendre en compte les virgules et les parenthèses
---
+++ calcul.js
@@ -0,0 +1,11 @@
+
+/*var keys = document.querySelectorAll("#calculator")*/
+
+
+// var num = parseInt(document.getElementById("num1").value);
+// var stock = parseInt(document.getElementById("affiche").value);
+//
+//   document.getElementById('num1').onclick = function(){
+//   return stock = num
+//
+// }
